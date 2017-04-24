d2aeef25a3e78eb106ee8917133bdd179f00c7a4
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-04-24 13:42:25 -0500

    Added some h1 tags to display results from conditions on Route

---
+++ resources/views/roll-dice.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Dice Roll</title>
+</head>
+<body>
+	<h1>Dice Roller</h1>
+	<h1><?= $message ?></h1>
+    <h2>Your guess: <?= $guess; ?></h1>
+    <h2>Dice Roll: <?= $diceRoll; ?></h1>
+</body>
+</html>
