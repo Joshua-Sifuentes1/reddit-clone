8eb3619faa2a2bcdc60b7e00f518b54f3ee0f66b
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-04-25 11:01:44 -0500

    Created posts table and votes table as per exexercise.

---
+++ database/migrations/2017_04_25_152359_create_posts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('posts', function(Blueprint $table){
+			// id  incrementing primary key
+			$table->increments('id');
+			// title   a short desciption of the post's content
+			$table->string('title');
+			// url a link to external content
+			$table->string('url');
+			// content several paragraphs of text
+			$table->string('description');
+			// created_by  a foreign key that references the user table
+			$table->integer('created_by')->unsigned();
+			$table->foreign('created_by')->references('id')->on('users');
+			// created_at  a datetime
+			// updated_at  a datetime
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('posts');
+	}
+}
---
+++ database/migrations/2017_04_25_153739_create_votes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVotesTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('votes', function(Blueprint $table){
+		// id incrementing primary key
+			$table->increments('id');
+		// user_id a foreign key that referencs the users table
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
+		// post_id a foreign key that references the posts table
+			$table->integer('post_id')->unsigned();
+			$table->foreign('post_id')->references('id')->on('posts');
+		// vote	a numeric representation of whether a user votes a post up or down
+			$table->integer('votes');
+		// created_at a datetime
+		// updated_at a datetime
+			$table->timestamps();			
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('votes');
+	}
+}
